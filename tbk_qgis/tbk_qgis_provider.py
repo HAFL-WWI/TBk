dfc66df8e0ceb52e16317d37b28f46cee15ea6a8
a-benini-2 <attilio.benini@bhf.ch>
2024-05-30 15:17:34 +0200

    feat: add plugin part / script for merging stand maps

--- tbk_qgis/tbk_qgis_provider.py
+++ tbk_qgis/tbk_qgis_provider.py
@@ -48,6 +48,7 @@ from tbk_qgis.tbk.preproc.tbk_qgis_prepare_all_algorithm import TBkPrepareAlgori
 from tbk_qgis.tbk.postproc.tbk_qgis_postprocess_local_density import TBkPostprocessLocalDensity
 from tbk_qgis.tbk.postproc.tbk_qgis_postprocess_cleanup import TBkPostprocessCleanup
 from tbk_qgis.tbk.postproc.tbk_qgis_postprocess_hdomDiff import TBkPostprocessHdomDiff
+from tbk_qgis.tbk.postproc.tbk_qgis_postprocess_merge_stand_maps import TBkPostprocessMergeStandMaps
 from tbk_qgis.tbk.postproc.tbk_qgis_postprocess_OSChange import TBkPostprocessOSChange
 from tbk_qgis.tbk.postproc.tbk_qgis_postprocess_wis2_export import TBkPostprocessWIS2Export
 from tbk_qgis.tbk.utility.optimized_spatial_join import OptimizedSpatialJoin
@@ -81,6 +82,7 @@ class TBkProvider(QgsProcessingProvider):
         # [grpID: postproc]     grpName: 2 Postprocessing
         self.addAlgorithm(TBkPostprocessCleanup())
         self.addAlgorithm(TBkPostprocessHdomDiff())
+        self.addAlgorithm(TBkPostprocessMergeStandMaps())
         self.addAlgorithm(TBkPostprocessOSChange())
         self.addAlgorithm(TBkPostprocessLocalDensity())
         self.addAlgorithm(TBkPostprocessWIS2Export())
