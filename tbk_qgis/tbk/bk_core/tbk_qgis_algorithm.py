b547854c486a85e6327313f15da51b479d77edf3
coutd1 <david.coutrot@students.bfh.ch>
2024-06-10 14:00:34 +0200

    feat: main algorithm modularization Proof of Concept

---
+++ tbk_qgis/tbk/bk_core/tbk_qgis_algorithm.py
@@ -0,0 +1,69 @@
+# todo: set header
+from tbk_qgis.tbk.bk_core.tbk_qgis_simplify_and_clean_algorithm import TBkSimplifyAndCleanAlgorithm
+from tbk_qgis.tbk.bk_core.tbk_qgis_processing_algorithm import TBkProcessingAlgorithm
+from tbk_qgis.tbk.bk_core.tbk_qgis_stand_delineation_algorithm import TBkStandDelineationAlgorithm
+import processing
+
+class TBkAlgorithm(TBkProcessingAlgorithm):
+    """
+    todo
+    """
+
+    # todo: store the algorithm modules in a list?
+    def __init__(self):
+        TBkProcessingAlgorithm.__init__(self)
+        self.stand_delineation_algorithm = TBkStandDelineationAlgorithm()
+        self.simplify_and_clean_algorithm = TBkSimplifyAndCleanAlgorithm()
+
+    # todo: avoid repetition
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along with some other properties.
+        """
+
+        self.stand_delineation_algorithm.initAlgorithm(config)
+        # Add the child algorithm's parameters to the main algorithm
+        for param in self.stand_delineation_algorithm.parameterDefinitions():
+            self.addParameter(param.clone())
+
+        self.simplify_and_clean_algorithm.initAlgorithm(config)
+
+        for param in self.simplify_and_clean_algorithm.parameterDefinitions():
+            if param.name() != 'working_root':
+                self.addParameter(param.clone())
+
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+
+        inputs = self.get_inputs(parameters, context)
+        outputs = self.stand_delineation_algorithm.processAlgorithm(inputs.__dict__, context, feedback)
+        inputs.working_root = outputs['WORKING_ROOT']
+        self.simplify_and_clean_algorithm.processAlgorithm(inputs.__dict__, context, feedback)
+
+        return {}
+
+    def createInstance(self):
+        """
+        Returns a new algorithm instance
+        """
+        return TBkAlgorithm()
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'Generate BK'
+
+    # todo
+    def shortHelpString(self):
+        """
+        Returns a localised short help string for the algorithm.
+        """
+        return ('')
--- tbk_qgis/tbk_qgis_provider.py
+++ tbk_qgis/tbk_qgis_provider.py
@@ -41,6 +41,7 @@ from PyQt5.QtGui import *
 
 
 from tbk_qgis.tbk.bk_core.tbk_qgis_algorithm_legagy import TBkAlgorithmLegacy
+from tbk_qgis.tbk.bk_core.tbk_qgis_algorithm import TBkAlgorithm
 from tbk_qgis.tbk.bk_ag.bkag_algorithm import BkAGAlgorithm
 from tbk_qgis.tbk.bk_core.tbk_qgis_stand_delineation_algorithm import TBkStandDelineationAlgorithm
 from tbk_qgis.tbk.bk_core.tbk_qgis_simplify_and_clean_algorithm import TBkSimplifyAndCleanAlgorithm
@@ -80,6 +81,7 @@ class TBkProvider(QgsProcessingProvider):
         # [grpID: core]         grpName: 1 Bk Generation
         self.addAlgorithm(TBkStandDelineationAlgorithm())
         self.addAlgorithm(TBkSimplifyAndCleanAlgorithm())
+        self.addAlgorithm(TBkAlgorithm())
         self.addAlgorithm(TBkAlgorithmLegacy())
         self.addAlgorithm(BkAGAlgorithm())
         # [grpID: postproc]     grpName: 2 Postprocessing
