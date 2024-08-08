5ce0a6708f5f480d0e1b81cf460afd5def4b3a1a
Hannes Horneber <hannes.horneber@bfh.ch>
2024-08-08 07:12:11 +0200

    fix: renamed one instance of class TBkPrepareVhmMgAlgorithm
    was TBkPrepareVhmMGAlgorithm. Seemed to work anyways, but PyCharm was complaining.
    Renamed for consistency.

--- tbk_qgis/tbk/preproc/tbk_qgis_prepare_vhm_mg_algorithm.py
+++ tbk_qgis/tbk/preproc/tbk_qgis_prepare_vhm_mg_algorithm.py
@@ -811,4 +811,4 @@ Notes:
 <p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p></body></html></p><br><p align="right">Algorithm authors: Attilio Benini, Hannes Horneber @ BFH-HAFL (2024)</p></body></html>"""
 
     def createInstance(self):
-        return TBkPrepareVhmMGAlgorithm()
+        return TBkPrepareVhmMgAlgorithm()
