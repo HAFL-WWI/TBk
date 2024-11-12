da1e05fdc55d6fce2fbd70fca5d0534715ee2cbb
Hannes Horneber <hannes.horneber@bfh.ch>
2024-11-12 16:57:43 +0100

    feat: function generate timestamp directory
    added to utility in preparation for new folder structure concept:
    
    \	tbk_YYYY	\	base_data_preprocessed \	*pp'd VHM, MG, forest_mask*
    				\	bk_process	 \	*tmp\, log.txt, ...*
    				\	dg_layers	 \	*dg_layer.tif ...*
    				\	TBk_Bestandeskarte.gpkg
    				\	TBk_Project.qgz
    
    				-- this section will only appear if processing regionwise
    				\	regions			\	*R1*\	bk_process	\	*tmp\, log.txt, ...*
    												TBk_Bestandeskarte.gpkg
    
    									\	*R2*\	bk_process	\	*tmp\, log.txt, ...*
    												TBk_Bestandeskarte.gpkg
    
    				-- these folders will only appear if run more than once
    				\	*X1*\	bk_process	\	*tmp\, log.txt, ...*
    							dg_layers	\	*dg_layer.tif ...*
    							regions		\	*R1*\ ...
    										\	*R2*\ ...
    							TBk_Bestandeskarte.gpkg
    							TBk_Project.qgz
    
    				\	*X2*\	bk_process	\	*tmp\, log.txt, ...*
    							dg_layers	\	*dg_layer.tif ...*
    							regions		\	*R1*\ ...
    										\	*R2*\ ...
    							TBk_Bestandeskarte.gpkg
    							TBk_Project.qgz

--- tbk_qgis/tbk/general/tbk_utilities.py
+++ tbk_qgis/tbk/general/tbk_utilities.py
@@ -3,6 +3,7 @@
 #
 # (C) Hannes Horneber, Dominique Weber, Christoph Schaller, HAFL
 #######################################################################
+from datetime import datetime
 
 from qgis import core
 from PyQt5.QtWidgets import *
@@ -19,13 +20,15 @@ from osgeo import ogr
 from osgeo import gdal
 from osgeo import osr
 
+
 # Remove fields from QGIS layer
 def delete_fields(layer, field_names):
     for f in field_names:
         fIndex = layer.fields().lookupField(f)
         if fIndex >= 0:
-            layer.dataProvider().deleteAttributes([fIndex]) 
+            layer.dataProvider().deleteAttributes([fIndex])
             layer.updateFields()
+
 
 # Remove fields from QGIS layer
 def keep_only_specific_fields(layer, fields_to_keep):
@@ -43,6 +46,7 @@ def keep_only_specific_fields(layer, fields_to_keep):
             layer.updateFields()
     # all_names_after = [field.name() for field in layer.dataProvider().fields()]
 
+
 def get_raster_metadata(raster):
     data = gdal.Open(raster, gdal.GA_ReadOnly)
     geoTransform = data.GetGeoTransform()
@@ -54,15 +58,18 @@ def get_raster_metadata(raster):
     xResolution = geoTransform[1]
     yResolution = geoTransform[5]
     data = None
-    return {"extent": [minx, miny, maxx, maxy], "xResolution": abs(xResolution), "yResolution": abs(yResolution), "epsg":proj.GetAttrValue('AUTHORITY',1)}
+    return {"extent": [minx, miny, maxx, maxy], "xResolution": abs(xResolution), "yResolution": abs(yResolution),
+            "epsg": proj.GetAttrValue('AUTHORITY', 1)}
+
 
 # Function that deletes an existing Shapefile
-#Based on https://pcjericks.github.io/py-gdalogr-cookbook/vector_layers.html#delete-a-file
+# Based on https://pcjericks.github.io/py-gdalogr-cookbook/vector_layers.html#delete-a-file
 def delete_shapefile(path):
     DriverName = "ESRI Shapefile"
     driver = ogr.GetDriverByName(DriverName)
     if os.path.exists(path):
         driver.DeleteDataSource(path)
+
 
 # Function that deletes an existing Geopackage
 def delete_geopackage(path):
@@ -70,6 +77,7 @@ def delete_geopackage(path):
     driver = ogr.GetDriverByName(DriverName)
     if os.path.exists(path):
         driver.DeleteDataSource(path)
+
 
 # Function that deletes an existing Geotiff
 def delete_raster(raster):
@@ -79,7 +87,8 @@ def delete_raster(raster):
     if os.path.exists(raster):
         driver.Delete(raster)
 
-#Function to copy a GeoTIFF raster
+
+# Function to copy a GeoTIFF raster
 def copy_raster_tiff(in_raster, out_raster):
     driver = gdal.GetDriverByName('GTiff')
     in_ds = gdal.Open(in_raster)
@@ -87,12 +96,13 @@ def copy_raster_tiff(in_raster, out_raster):
     in_ds = None
     out_ds = None
 
-#Function to create and empty copy of a GeoTIFF raster
+
+# Function to create and empty copy of a GeoTIFF raster
 def create_empty_copy(input_raster, output_raster):
     in_ds = gdal.Open(input_raster)
     driver = in_ds.GetDriver()
     out_ds = driver.Create(output_raster, in_ds.RasterXSize, in_ds.RasterYSize, in_ds.RasterCount,
-                       in_ds.GetRasterBand(1).DataType)
+                           in_ds.GetRasterBand(1).DataType)
     out_ds.SetGeoTransform(in_ds.GetGeoTransform())
     out_ds.SetProjection(in_ds.GetProjection())
     ds = None
@@ -106,21 +116,24 @@ def delete_shapefile_old(path):
     aDir, aFile = os.path.split(realpath)
     fnameNoExt = os.path.splitext(aFile)[0]
 
-    extensions = [".shp", ".shx", ".dbf", ".prj", ".sbn", ".sbx", ".fbn", ".fbx", ".ain", ".aih", ".ixs", ".mxs", ".atx", ".xml", ".cpg", ".qix"]
+    extensions = [".shp", ".shx", ".dbf", ".prj", ".sbn", ".sbx", ".fbn", ".fbx", ".ain", ".aih", ".ixs", ".mxs",
+                  ".atx", ".xml", ".cpg", ".qix"]
 
     theFiles = []
     for f in os.listdir(aDir):
         if os.path.isfile(os.path.join(aDir, f)):
             theFiles.append(os.path.join(aDir, f))
-            
+
     for f in theFiles:
         theFile = os.path.basename(f)
         name, extension = os.path.splitext(theFile)
         # If the name matches the input file and the extension is in that list, delete it:
-        if (name == fnameNoExt or name == fnameNoExt + ".shp") and (extension in extensions): # handles the foo.shp.xml case too.
+        if (name == fnameNoExt or name == fnameNoExt + ".shp") and (
+                extension in extensions):  # handles the foo.shp.xml case too.
             os.remove(f)
 
-def ensure_dir (path):
+
+def ensure_dir(path):
     """Function to ensure that a directory exists
     (creates directory if non existent)
 
@@ -129,14 +142,16 @@ def ensure_dir (path):
     if not os.path.isdir(path):
         return os.makedirs(path, exist_ok=True)
 
-def getVectorSaveOptions(format, encoding, only_selected_features = False, in_crs = None, out_crs = None):
+
+def getVectorSaveOptions(format, encoding, only_selected_features=False, in_crs=None, out_crs=None):
     save_options = QgsVectorFileWriter.SaveVectorOptions()
     save_options.driverName = format
     save_options.fileEncoding = encoding
     save_options.onlySelectedFeatures = only_selected_features
     if in_crs and out_crs:
-        save_options.ct =  QgsCoordinateTransform(in_crs, out_crs, QgsProject.instance())
+        save_options.ct = QgsCoordinateTransform(in_crs, out_crs, QgsProject.instance())
     return save_options
+
 
 def dict_diff(a, b):
     """
@@ -168,3 +183,19 @@ def dict_diff(a, b):
         if key not in a:
             added[key] = value
     return removed, added, changed
+
+
+def generate_timestamp_dir(root_dir, prefix='', suffix=''):
+    """
+    Generate a directory with current timestamp in root_dir
+    The directory name may contain a prefix/suffix in addition to the timestamp
+    """
+    # Set up timestamp and directory name and path
+    time = datetime.now().strftime("%Y%m%d-%H%M")
+    dir_name = f"{prefix}{time}{suffix}"
+    new_directory = os.path.join(root_dir, dir_name)
+
+    # create directory
+    ensure_dir(new_directory)
+
+    return new_directory
