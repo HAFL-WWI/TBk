2d92f437b7dde7fb014a340552129a7578eb1ac8
Hannes Horneber <hannes.horneber@bfh.ch>
2025-01-09 13:51:28 +0100

    feature: add function with graceful error handling for os.remove
    also rearranged the utility functions

--- tbk_qgis/tbk/utility/tbk_utilities.py
+++ tbk_qgis/tbk/utility/tbk_utilities.py
@@ -42,13 +42,126 @@ from osgeo import ogr
 from osgeo import gdal
 from osgeo import osr
 
+
+def ensure_dir(path):
+    """Function to ensure that a directory exists
+    (creates directory if non existent)
+
+    :param path: Path of directory to check
+    """
+    if not os.path.isdir(path):
+        return os.makedirs(path, exist_ok=True)
+
+
+# File removal with graceful error handling (avoid error interrupts)
+def os_remove_graceful(file_path):
+    if file_path:  # Ensure the variable is not None or empty
+        if os.path.exists(file_path):  # Check if the file exists
+            try:
+                os.remove(file_path)
+                print(f"Deleted: {file_path}")
+            except Exception as e:
+                print(f"Could not delete {file_path}: {e}")
+        else:
+            print(f"File does not exist, skipping: {file_path}")
+    else:
+        print("No file path provided, skipping.")
+
+
+# Function that deletes an existing Geotiff
+def delete_raster(raster):
+    data = gdal.Open(raster, gdal.GA_ReadOnly)
+    driver = data.GetDriver()
+    data = None
+    if os.path.exists(raster):
+        driver.Delete(raster)
+
+
+# Function that deletes an existing Geopackage
+def delete_geopackage(path):
+    DriverName = "GPKG"
+    driver = ogr.GetDriverByName(DriverName)
+    if os.path.exists(path):
+        driver.DeleteDataSource(path)
+
+
+# Function that deletes an existing Shapefile
+# Based on https://pcjericks.github.io/py-gdalogr-cookbook/vector_layers.html#delete-a-file
+def delete_shapefile(path):
+    DriverName = "ESRI Shapefile"
+    driver = ogr.GetDriverByName(DriverName)
+    if os.path.exists(path):
+        driver.DeleteDataSource(path)
+
+
+# Delete Shapefile
+# Code based on https://gis.stackexchange.com/a/190435
+def delete_shapefile_old(path):
+    realpath = os.path.realpath(path)
+    aDir, aFile = os.path.split(realpath)
+    fnameNoExt = os.path.splitext(aFile)[0]
+
+    extensions = [".shp", ".shx", ".dbf", ".prj", ".sbn", ".sbx", ".fbn", ".fbx", ".ain", ".aih", ".ixs", ".mxs",
+                  ".atx", ".xml", ".cpg", ".qix"]
+
+    theFiles = []
+    for f in os.listdir(aDir):
+        if os.path.isfile(os.path.join(aDir, f)):
+            theFiles.append(os.path.join(aDir, f))
+
+    for f in theFiles:
+        theFile = os.path.basename(f)
+        name, extension = os.path.splitext(theFile)
+        # If the name matches the input file and the extension is in that list, delete it:
+        if (name == fnameNoExt or name == fnameNoExt + ".shp") and (
+                extension in extensions):  # handles the foo.shp.xml case too.
+            os.remove(f)
+
+
+def get_raster_metadata(raster):
+    data = gdal.Open(raster, gdal.GA_ReadOnly)
+    geoTransform = data.GetGeoTransform()
+    minx = geoTransform[0]
+    maxy = geoTransform[3]
+    maxx = minx + geoTransform[1] * data.RasterXSize
+    miny = maxy + geoTransform[5] * data.RasterYSize
+    proj = osr.SpatialReference(wkt=data.GetProjection())
+    xResolution = geoTransform[1]
+    yResolution = geoTransform[5]
+    data = None
+    return {"extent": [minx, miny, maxx, maxy], "xResolution": abs(xResolution), "yResolution": abs(yResolution),
+            "epsg": proj.GetAttrValue('AUTHORITY', 1)}
+
+
+# Function to copy a GeoTIFF raster
+def copy_raster_tiff(in_raster, out_raster):
+    driver = gdal.GetDriverByName('GTiff')
+    in_ds = gdal.Open(in_raster)
+    out_ds = driver.CreateCopy(out_raster, in_ds, 0, options=["COMPRESS=ZSTD"])
+    in_ds = None
+    out_ds = None
+
+
+# Function to create and empty copy of a GeoTIFF raster
+def create_empty_copy(input_raster, output_raster):
+    in_ds = gdal.Open(input_raster)
+    driver = in_ds.GetDriver()
+    out_ds = driver.Create(output_raster, in_ds.RasterXSize, in_ds.RasterYSize, in_ds.RasterCount,
+                           in_ds.GetRasterBand(1).DataType)
+    out_ds.SetGeoTransform(in_ds.GetGeoTransform())
+    out_ds.SetProjection(in_ds.GetProjection())
+    ds = None
+    out_ds = None
+
+
 # Remove fields from QGIS layer
 def delete_fields(layer, field_names):
     for f in field_names:
         fIndex = layer.fields().lookupField(f)
         if fIndex >= 0:
-            layer.dataProvider().deleteAttributes([fIndex]) 
+            layer.dataProvider().deleteAttributes([fIndex])
             layer.updateFields()
+
 
 # Remove fields from QGIS layer
 def keep_only_specific_fields(layer, fields_to_keep):
@@ -66,100 +179,16 @@ def keep_only_specific_fields(layer, fields_to_keep):
             layer.updateFields()
     # all_names_after = [field.name() for field in layer.dataProvider().fields()]
 
-def get_raster_metadata(raster):
-    data = gdal.Open(raster, gdal.GA_ReadOnly)
-    geoTransform = data.GetGeoTransform()
-    minx = geoTransform[0]
-    maxy = geoTransform[3]
-    maxx = minx + geoTransform[1] * data.RasterXSize
-    miny = maxy + geoTransform[5] * data.RasterYSize
-    proj = osr.SpatialReference(wkt=data.GetProjection())
-    xResolution = geoTransform[1]
-    yResolution = geoTransform[5]
-    data = None
-    return {"extent": [minx, miny, maxx, maxy], "xResolution": abs(xResolution), "yResolution": abs(yResolution), "epsg":proj.GetAttrValue('AUTHORITY',1)}
 
-# Function that deletes an existing Shapefile
-#Based on https://pcjericks.github.io/py-gdalogr-cookbook/vector_layers.html#delete-a-file
-def delete_shapefile(path):
-    DriverName = "ESRI Shapefile"
-    driver = ogr.GetDriverByName(DriverName)
-    if os.path.exists(path):
-        driver.DeleteDataSource(path)
-
-# Function that deletes an existing Geopackage
-def delete_geopackage(path):
-    DriverName = "GPKG"
-    driver = ogr.GetDriverByName(DriverName)
-    if os.path.exists(path):
-        driver.DeleteDataSource(path)
-
-# Function that deletes an existing Geotiff
-def delete_raster(raster):
-    data = gdal.Open(raster, gdal.GA_ReadOnly)
-    driver = data.GetDriver()
-    data = None
-    if os.path.exists(raster):
-        driver.Delete(raster)
-
-#Function to copy a GeoTIFF raster
-def copy_raster_tiff(in_raster, out_raster):
-    driver = gdal.GetDriverByName('GTiff')
-    in_ds = gdal.Open(in_raster)
-    out_ds = driver.CreateCopy(out_raster, in_ds, 0, options=["COMPRESS=ZSTD"])
-    in_ds = None
-    out_ds = None
-
-#Function to create and empty copy of a GeoTIFF raster
-def create_empty_copy(input_raster, output_raster):
-    in_ds = gdal.Open(input_raster)
-    driver = in_ds.GetDriver()
-    out_ds = driver.Create(output_raster, in_ds.RasterXSize, in_ds.RasterYSize, in_ds.RasterCount,
-                       in_ds.GetRasterBand(1).DataType)
-    out_ds.SetGeoTransform(in_ds.GetGeoTransform())
-    out_ds.SetProjection(in_ds.GetProjection())
-    ds = None
-    out_ds = None
-
-
-# Delete files
-# Code basing on https://gis.stackexchange.com/a/190435
-def delete_shapefile_old(path):
-    realpath = os.path.realpath(path)
-    aDir, aFile = os.path.split(realpath)
-    fnameNoExt = os.path.splitext(aFile)[0]
-
-    extensions = [".shp", ".shx", ".dbf", ".prj", ".sbn", ".sbx", ".fbn", ".fbx", ".ain", ".aih", ".ixs", ".mxs", ".atx", ".xml", ".cpg", ".qix"]
-
-    theFiles = []
-    for f in os.listdir(aDir):
-        if os.path.isfile(os.path.join(aDir, f)):
-            theFiles.append(os.path.join(aDir, f))
-            
-    for f in theFiles:
-        theFile = os.path.basename(f)
-        name, extension = os.path.splitext(theFile)
-        # If the name matches the input file and the extension is in that list, delete it:
-        if (name == fnameNoExt or name == fnameNoExt + ".shp") and (extension in extensions): # handles the foo.shp.xml case too.
-            os.remove(f)
-
-def ensure_dir (path):
-    """Function to ensure that a directory exists
-    (creates directory if non existent)
-
-    :param path: Path of directory to check
-    """
-    if not os.path.isdir(path):
-        return os.makedirs(path, exist_ok=True)
-
-def getVectorSaveOptions(format, encoding, only_selected_features = False, in_crs = None, out_crs = None):
+def getVectorSaveOptions(format, encoding, only_selected_features=False, in_crs=None, out_crs=None):
     save_options = QgsVectorFileWriter.SaveVectorOptions()
     save_options.driverName = format
     save_options.fileEncoding = encoding
     save_options.onlySelectedFeatures = only_selected_features
     if in_crs and out_crs:
-        save_options.ct =  QgsCoordinateTransform(in_crs, out_crs, QgsProject.instance())
+        save_options.ct = QgsCoordinateTransform(in_crs, out_crs, QgsProject.instance())
     return save_options
+
 
 def dict_diff(a, b):
     """
