7580ea2c32862e48ab35cc67ed96fdf1979a3ca6
davidcout2 <161458878+davidcout2@users.noreply.github.com>
2025-04-11 11:17:12 +0200

    refactor simplify tool: rename variable to meet python snake case naming convention

--- tbk_qgis/tbk/tools/C_stand_delineation/bk_hafl_post_process.py
+++ tbk_qgis/tbk/tools/C_stand_delineation/bk_hafl_post_process.py
@@ -42,7 +42,7 @@ def post_process(shape_in, h_max_input, shape_out, tmp_output_folder, min_area, 
     expression = "area_m2 < " + str(min_area)
 
     # Expression to calculate area m2
-    eArea = "{0}".format("to_int(area($geometry))")
+    area_expression = "{0}".format("to_int(area($geometry))")
 
     # Temp File names
     tmp_files_names = {
@@ -66,18 +66,18 @@ def post_process(shape_in, h_max_input, shape_out, tmp_output_folder, min_area, 
 
     params = {'INPUT_RASTER': h_max_input, 'RASTER_BAND': 1, 'FIELD_NAME': 'VALUE',
               'OUTPUT': 'TEMPORARY_OUTPUT'}
-    algoOutput = processing.run("native:pixelstopoints", params)
+    algo_output = processing.run("native:pixelstopoints", params)
 
-    params = {'INPUT': algoOutput["OUTPUT"], 'FIELD': 'VALUE', 'OPERATOR': 2, 'VALUE': '0',
+    params = {'INPUT': algo_output["OUTPUT"], 'FIELD': 'VALUE', 'OPERATOR': 2, 'VALUE': '0',
               'OUTPUT': output_files['stands_highest_tree']}
-    algoOutput = processing.run("native:extractbyattribute", params)
+    processing.run("native:extractbyattribute", params)
 
     ########################################
     # --- Eliminate small polygons
 
     params = {'INPUT': shape_in, 'DISTANCE': 0, 'SEGMENTS': 5, 'END_CAP_STYLE': 0, 'JOIN_STYLE': 0,
               'MITER_LIMIT': 2, 'DISSOLVE': False, 'OUTPUT': tmp_files['stands_buf']}
-    algoOutput = processing.run("native:buffer", params)
+    processing.run("native:buffer", params)
 
     stand_boundaries_layer = QgsVectorLayer(tmp_files['stands_buf'], "stand_boundaries", "ogr")
     # Execute SelectLayerByAttribute to define features to be eliminated
@@ -89,12 +89,12 @@ def post_process(shape_in, h_max_input, shape_out, tmp_output_folder, min_area, 
 
     # Does not persist results when writing directly to file
     param = {'INPUT': stand_boundaries_layer, 'MODE': 2, 'OUTPUT': 'memory:'}
-    algoOutput = processing.run("qgis:eliminateselectedpolygons", param)
+    algo_output = processing.run("qgis:eliminateselectedpolygons", param)
 
     ctc = QgsProject.instance().transformContext()
-    QgsVectorFileWriter.writeAsVectorFormatV3(algoOutput['OUTPUT'], tmp_files['reduced'], ctc,
+    QgsVectorFileWriter.writeAsVectorFormatV3(algo_output['OUTPUT'], tmp_files['reduced'], ctc,
                                               getVectorSaveOptions('GPKG', 'utf-8'))
-    # QgsVectorFileWriter.writeAsVectorFormatV3(algoOutput['OUTPUT'],tmp_reduced_path,"utf-8",stand_boundaries_layer.sourceCrs(),"GPKG")
+    # QgsVectorFileWriter.writeAsVectorFormatV3(algo_output['OUTPUT'],tmp_reduced_path,"utf-8",stand_boundaries_layer.sourceCrs(),"GPKG")
 
     ########################################
     # --- Simplify
@@ -106,16 +106,16 @@ def post_process(shape_in, h_max_input, shape_out, tmp_output_folder, min_area, 
              '-t': False, '-l': True, 'output': 'TEMPORARY_OUTPUT', 'error': tmp_files['simplified_error'],
              'GRASS_REGION_PARAMETER': None, 'GRASS_SNAP_TOLERANCE_PARAMETER': -1, 'GRASS_MIN_AREA_PARAMETER': 0.0001,
              'GRASS_OUTPUT_TYPE_PARAMETER': 0, 'GRASS_VECTOR_DSCO': '', 'GRASS_VECTOR_LCO': ''}
-    algoOutput = processing.run("grass7:v.generalize", param)
+    algo_output = processing.run("grass7:v.generalize", param)
 
     # a second simplify pass, since the first pass leaves some stands unchanged
-    param = {'input': algoOutput['output'], 'type': [0, 1, 2], 'cats': '', 'where': '', 'method': 0,
+    param = {'input': algo_output['output'], 'type': [0, 1, 2], 'cats': '', 'where': '', 'method': 0,
              'threshold': simplification_tolerance, 'look_ahead': 7, 'reduction': 50, 'slide': 0.5, 'angle_thresh': 3,
              'degree_thresh': 0, 'closeness_thresh': 0, 'betweeness_thresh': 0, 'alpha': 1, 'beta': 1, 'iterations': 1,
              '-t': False, '-l': True, 'output': tmp_files['simplified'], 'error': tmp_files['simplified_error'],
              'GRASS_REGION_PARAMETER': None, 'GRASS_SNAP_TOLERANCE_PARAMETER': -1, 'GRASS_MIN_AREA_PARAMETER': 0.0001,
              'GRASS_OUTPUT_TYPE_PARAMETER': 0, 'GRASS_VECTOR_DSCO': '', 'GRASS_VECTOR_LCO': ''}
-    algoOutput = processing.run("grass7:v.generalize", param)
+    processing.run("grass7:v.generalize", param)
 
     tmp_simplified_layer = QgsVectorLayer(tmp_files['simplified'], "stand_boundaries_reduced", "ogr")
 
@@ -128,15 +128,15 @@ def post_process(shape_in, h_max_input, shape_out, tmp_output_folder, min_area, 
     # --- Recalculate area
     print("recalculating area...")
     param = {'INPUT': tmp_files['simplified'], 'OUTPUT': 'memory:'}
-    algoOutput = processing.run("native:fixgeometries", param)
+    algo_output = processing.run("native:fixgeometries", param)
 
-    param = {'INPUT': algoOutput['OUTPUT'], 'FIELD_NAME': 'area_m2', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
-             'FIELD_PRECISION': 3, 'NEW_FIELD': False, 'FORMULA': eArea, 'OUTPUT': 'memory:'}
-    algoOutput = processing.run("qgis:fieldcalculator", param)
+    param = {'INPUT': algo_output['OUTPUT'], 'FIELD_NAME': 'area_m2', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
+             'FIELD_PRECISION': 3, 'NEW_FIELD': False, 'FORMULA': area_expression, 'OUTPUT': 'memory:'}
+    algo_output = processing.run("qgis:fieldcalculator", param)
 
     del tmp_simplified_layer
 
-    QgsVectorFileWriter.writeAsVectorFormatV3(algoOutput['OUTPUT'], tmp_files['simplified'], ctc,
+    QgsVectorFileWriter.writeAsVectorFormatV3(algo_output['OUTPUT'], tmp_files['simplified'], ctc,
                                               getVectorSaveOptions('GPKG', 'utf-8'))
 
     ########################################
@@ -156,67 +156,67 @@ def post_process(shape_in, h_max_input, shape_out, tmp_output_folder, min_area, 
 
     # Does not persist results when writing directly to file
     param = {'INPUT': tmp_simplified_layer, 'MODE': 2, 'OUTPUT': 'memory:'}
-    algoOutput = processing.run("qgis:eliminateselectedpolygons", param)
+    algo_output = processing.run("qgis:eliminateselectedpolygons", param)
 
-    QgsVectorFileWriter.writeAsVectorFormatV3(algoOutput['OUTPUT'], tmp_files['reduced'], ctc,
+    QgsVectorFileWriter.writeAsVectorFormatV3(algo_output['OUTPUT'], tmp_files['reduced'], ctc,
                                               getVectorSaveOptions('GPKG', 'utf-8'))
 
     ########################################
     # --- Recalculate area
     print("recalculating area...")
-    param = {'INPUT': algoOutput['OUTPUT'], 'FIELD_NAME': 'area_m2', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
-             'FIELD_PRECISION': 3, 'NEW_FIELD': False, 'FORMULA': eArea, 'OUTPUT': 'memory:'}
-    algoOutput = processing.run("qgis:fieldcalculator", param)
+    param = {'INPUT': algo_output['OUTPUT'], 'FIELD_NAME': 'area_m2', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
+             'FIELD_PRECISION': 3, 'NEW_FIELD': False, 'FORMULA': area_expression, 'OUTPUT': 'memory:'}
+    algo_output = processing.run("qgis:fieldcalculator", param)
 
     del tmp_simplified_layer
-    # QgsVectorFileWriter.writeAsVectorFormatV3(algoOutput['OUTPUT'],tmp_simplified_path,ctc,getVectorSaveOptions('GPKG','utf-8'))
+    # QgsVectorFileWriter.writeAsVectorFormatV3(algo_output['OUTPUT'],tmp_simplified_path,ctc,getVectorSaveOptions('GPKG','utf-8'))
 
     ########################################
     # --- Update hmax and hdom for remainders
     print("filling in hmax and hdom for remainders...")
     # Create tmp layer
     # tmp_simplified_layer = QgsVectorLayer(tmp_simplified_path, "stand_boundaries_simplified", "ogr")
-    tmp_simplified_layer = algoOutput['OUTPUT']
+    tmp_simplified_layer = algo_output['OUTPUT']
 
     # Select remainders and calculate hmax, hdom
     param = {'INPUT': tmp_simplified_layer, 'FIELD': 'type', 'OPERATOR': 0, 'VALUE': 'remainder', 'METHOD': 0}
-    algoOutput = processing.run("qgis:selectbyattribute", param)
+    algo_output = processing.run("qgis:selectbyattribute", param)
 
     # update hmax attribute (with value of hmax_eff) for selected stands
-    param = {'INPUT': algoOutput['OUTPUT'], 'FIELD_NAME': 'hmax', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
+    param = {'INPUT': algo_output['OUTPUT'], 'FIELD_NAME': 'hmax', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
              'FIELD_PRECISION': 3, 'NEW_FIELD': False, 'FORMULA': 'if(is_selected(),hmax_eff,hmax)',
              'OUTPUT': 'memory:'}
-    algoOutput = processing.run("qgis:fieldcalculator", param)
+    algo_output = processing.run("qgis:fieldcalculator", param)
 
     # Select remainders and calculate hmax, hdom
-    param = {'INPUT': algoOutput['OUTPUT'], 'FIELD': 'type', 'OPERATOR': 0, 'VALUE': 'remainder', 'METHOD': 0}
-    algoOutput = processing.run("qgis:selectbyattribute", param)
+    param = {'INPUT': algo_output['OUTPUT'], 'FIELD': 'type', 'OPERATOR': 0, 'VALUE': 'remainder', 'METHOD': 0}
+    algo_output = processing.run("qgis:selectbyattribute", param)
 
     # update hmax attribute (with value of hp_80) for selected stands
-    param = {'INPUT': algoOutput['OUTPUT'], 'FIELD_NAME': 'hdom', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
+    param = {'INPUT': algo_output['OUTPUT'], 'FIELD_NAME': 'hdom', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
              'FIELD_PRECISION': 3, 'NEW_FIELD': False, 'FORMULA': 'if(is_selected(),hp80,hdom)', 'OUTPUT': 'memory:'}
-    algoOutput = processing.run("qgis:fieldcalculator", param)
+    algo_output = processing.run("qgis:fieldcalculator", param)
 
     # Delete fields
     fields = ['hmax_eff', 'hp80']
     if del_tmp:
-        delete_fields(algoOutput['OUTPUT'], fields)
+        delete_fields(algo_output['OUTPUT'], fields)
 
     ########################################
     # # create Field "FID_orig"
     # # Prepare for further analysis of neighbours
-    # param = {'INPUT': algoOutput['OUTPUT'], 'FIELD_NAME': 'FID_orig', 'FIELD_TYPE': 1, 'FIELD_LENGTH': 10,
+    # param = {'INPUT': algo_output['OUTPUT'], 'FIELD_NAME': 'FID_orig', 'FIELD_TYPE': 1, 'FIELD_LENGTH': 10,
     #          'FIELD_PRECISION': 0, 'OUTPUT': 'memory:'}
-    # algoOutput = processing.run("qgis:addfieldtoattributestable", param)
+    # algo_output = processing.run("qgis:addfieldtoattributestable", param)
     # ##May be the wrong field!
-    # param = {'INPUT': algoOutput['OUTPUT'], 'FIELD_NAME': 'FID_orig', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
+    # param = {'INPUT': algo_output['OUTPUT'], 'FIELD_NAME': 'FID_orig', 'FIELD_TYPE': 0, 'FIELD_LENGTH': 10,
     #          'FIELD_PRECISION': 3, 'NEW_FIELD': False, 'FORMULA': 'OBJECTID', 'OUTPUT': 'memory:'}
-    # algoOutput = processing.run("qgis:fieldcalculator", param)
+    # algo_output = processing.run("qgis:fieldcalculator", param)
 
     ########################################
 
     # finally persist output
-    QgsVectorFileWriter.writeAsVectorFormatV3(algoOutput['OUTPUT'], output_files["stands_simplified"], ctc,
+    QgsVectorFileWriter.writeAsVectorFormatV3(algo_output['OUTPUT'], output_files["stands_simplified"], ctc,
                                               getVectorSaveOptions('GPKG', 'utf-8'))
 
     # Delete files
